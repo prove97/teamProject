079fd1ee6cdcc92d61af57b463ed1fb72829be40
yongjoon <missjj312@naver.com>
2024-04-12 16:51:04 +0900

    수정

--- src/main/java/com/two/category/model/vo/Category.java
+++ src/main/java/com/two/category/model/vo/Category.java
@@ -2,4 +2,50 @@ package com.two.category.model.vo;
 
 public class Category {
 
+	private String categortId;
+	private String categoryM;
+	private String categoryT;
+	
+	public Category() {
+		super();
+	}
+
+	public Category(String categortId, String categoryM, String categoryT) {
+		super();
+		this.categortId = categortId;
+		this.categoryM = categoryM;
+		this.categoryT = categoryT;
+	}
+
+	public String getCategortId() {
+		return categortId;
+	}
+
+	public void setCategortId(String categortId) {
+		this.categortId = categortId;
+	}
+
+	public String getCategoryM() {
+		return categoryM;
+	}
+
+	public void setCategoryM(String categoryM) {
+		this.categoryM = categoryM;
+	}
+
+	public String getCategoryT() {
+		return categoryT;
+	}
+
+	public void setCategoryT(String categoryT) {
+		this.categoryT = categoryT;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [categortId=" + categortId + ", categoryM=" + categoryM + ", categoryT=" + categoryT + "]";
+	}
+	
+	
+	
 }
