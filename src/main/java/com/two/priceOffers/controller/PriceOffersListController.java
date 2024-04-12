adf6c7908174252a68c5d8d556dc1142b2eadb17
DoHeonKim <kdh030777@gmail.com>
2024-04-12 17:10:23 +0900

    servlet 네이밍 수정

--- src/main/java/com/two/priceOffers/controller/PriceOffersListController.java
+++ src/main/java/com/two/priceOffers/controller/PriceOffersListController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class priceOffersListController
  */
-@WebServlet("/list.pr")
+@WebServlet("/list.of")
 public class PriceOffersListController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
