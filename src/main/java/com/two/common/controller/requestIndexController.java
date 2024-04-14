527fa73a414d6354ab121e23cc6520af8bd6a045
DoHeon Kim <kdh030777@gmail.com>
2024-04-14 13:19:03 +0900

    feat index에서 myPage로 이동, 헤더 수정, css파일 분리

--- src/main/java/com/two/common/controller/requestIndexController.java
+++ src/main/java/com/two/common/controller/requestIndexController.java
@@ -1,4 +1,4 @@
-package com.two.member.controller;
+package com.two.common.controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class MemberEnrollFormController
+ * Servlet implementation class requestIndexController
  */
-@WebServlet("/Login.me")
-public class MemberloginController extends HttpServlet {
+@WebServlet("/index.re")
+public class requestIndexController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public MemberloginController() {
+    public requestIndexController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,7 +26,7 @@ public class MemberloginController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/views/login/login.jsp").forward(request, response);
+			request.getRequestDispatcher("index.jsp").forward(request, response);
 	}
 
 	/**
