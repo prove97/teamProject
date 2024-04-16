4f4700159586f3b4b67d1b2935a39f5d97f8abcf
prove97 <dsr102938@gmail.com>
2024-04-16 17:48:42 +0900

    [이원기] 아이디 중복 확인

--- src/main/java/com/two/myPage/model/dao/MyPageDao.java
+++ src/main/java/com/two/myPage/model/dao/MyPageDao.java
@@ -1,5 +1,14 @@
 package com.two.myPage.model.dao;
 
+import org.apache.ibatis.session.SqlSession;
+
+import com.two.member.model.vo.Member;
+
 public class MyPageDao {
+	
+	public Member nicknameCheck(SqlSession sqlSession, String nickname) {			
+		Member m = sqlSession.selectOne("memberMapper.selectMember", nickname); //닉네임이 일치하는 Member객체 리턴 (mapper수정해야함)
+		return m;
+	}
 
 }
--- src/main/java/com/two/myPage/service/MyPageServiceImpl.java
+++ src/main/java/com/two/myPage/service/MyPageServiceImpl.java
@@ -1,5 +1,27 @@
 package com.two.myPage.service;
 
-public class MyPageServiceImpl {
+import org.apache.ibatis.session.SqlSession;
+
+import com.two.common.Template;
+import com.two.member.model.vo.Member;
+import com.two.myPage.model.dao.MyPageDao;
+
+public class MyPageServiceImpl implements MyPageService{
+
+	@Override
+	public int nicknameCheck(String nickname) {
+		SqlSession sqlSession = Template.getSqlSession();
+		
+		Member m = new MyPageDao().nicknameCheck(sqlSession, nickname);
+		int count = 0;  
+		
+		if(m != null) { //일치하는 닉네임이 있는경우
+			count = 1; 
+		}
+		
+		sqlSession.close();
+
+		return count;
+	}
 	
 }
