c797e0c6e37f9a5238cccf78a7a48918225b46b3
DoHeonKim <kdh030777@gmail.com>
2024-04-23 17:21:24 +0900

    [김도헌] index의 js 파일 수정

--- src/main/webapp/script/index.js
+++ src/main/webapp/script/index.js
@@ -1,11 +1,62 @@
 function contentsFunction(cpage){
-        	console.log("ajax 실행됨")
-            $.ajax({
-                url: "list.pr",
-                dataType:"json",
-                data: {'cpage' : cpage},
-                success: function(data){
-                    console.log(data)
+			//검색창 가져와서 값가져오기
+			//셀렉트박스가져와서 값가져오기
+
+		const searchValue = document.getElementById("searchKeyword").value;
+		console.log(searchValue.keyCode)
+		if(searchValue == ""){
+			getList({cpage: cpage}, function(data){
+				drawList(data)
+			})
+		}else {
+			getSearchList({cpage : cpage, searchValue : searchValue}, function(data){
+				drawList(data)
+			})
+		}
+
+}
+
+function enter(e){
+	console.log(e.code)
+	if(e.code=='Enter'){
+		contentsFunction(1);
+	}
+}
+
+function getList(data, callback){
+	console.log("ajax 실행됨")
+        $.ajax({
+            url: "list.pr",
+            dataType:"json",
+            data: data,
+            success: function(data){
+                callback(data)
+               
+            },
+            error: function(){
+                console.log("ajax요청실패")
+            }
+    })
+}
+
+function getSearchList(data, callback){
+		$.ajax({
+	            url: "search.pr",
+	            dataType:"json",
+	            data: data,
+	            success: function(data){
+	                callback(data)
+	               
+	            },
+	            error: function(){
+	                console.log("ajax요청실패")
+	            }
+	    })
+}
+
+
+function drawList(data){
+	console.log(data)
                     let str = "";
                     let Section = document.getElementById("contents");
                     for(const p of data.list){
@@ -43,26 +94,20 @@ function contentsFunction(cpage){
                     let pagingStr = "";
 
                     if (data.pi.currentPage != 1) {
-                        pagingStr += `<button onclick="contentsFunction(\${data.pi.currentPage - 1 });">&lt;</button>`;
+                        pagingStr += `<button onclick="contentsFunction(${data.pi.currentPage - 1 });">&lt;</button>`;
                     }
                     for (let p = data.pi.startPage; p <= data.pi.endPage; p++) {
                         if (p == data.pi.currentPage) {
                             pagingStr += `<button disabled>`+p+`</button>`;
                         } else {
-                            pagingStr += `<button onclick="contentsFunction(\${p});">`+p+`</button>`;
+                            pagingStr += `<button onclick="contentsFunction(${p});">`+p+`</button>`;
                         }
                     }
                     if (data.pi.currentPage != data.pi.maxPage) {
-                        pagingStr += `<button onclick="contentsFunction(\${data.pi.currentPage + 1 });">&gt;</button>`;
+                        pagingStr += `<button onclick="contentsFunction(${data.pi.currentPage + 1 });">&gt;</button>`;
                     }
 
                     pagi.innerHTML = pagingStr;
-                   
-                },
-                error: function(){
-                    console.log("ajax요청실패")
-                }
-        })
 }
 
 
