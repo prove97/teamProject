b441cd6ecb3dc37adf87341672c19712b5ef6443
DoHeonKim <kdh030777@gmail.com>
2024-04-24 17:43:54 +0900

    [김도헌] 병준님도와주기

---
+++ src/main/java/com/two/product/controller/ProductDeleteController.java
@@ -0,0 +1,48 @@
+package com.two.product.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.two.product.service.ProductServiceImpl;
+
+/**
+ * Servlet implementation class ProductDeleteController
+ */
+@WebServlet("/delete.pr")
+public class ProductDeleteController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProductDeleteController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int goodsId = Integer.parseInt(request.getParameter("goodsId"));
+		
+		int result = new ProductServiceImpl().deleteProduct(goodsId);
+		
+		if(result > 0) {
+			request.getAttribute("alter", "dfasdf");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
